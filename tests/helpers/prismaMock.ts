9220ead96edf4437cb140e43f65932bccaf6ee83
Sasha Reid <github@sasha.hackl.es>
2023-01-12 15:09:04 -0800

    Database helper tests galore

--- tests/helpers/prismaMock.ts
+++ tests/helpers/prismaMock.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, EligibilityFormPage, Prisma } from "@prisma/client";
 import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
 import db from "app/utils/db.connection";
 
@@ -12,3 +12,13 @@ beforeEach(() => {
 });
 
 export const prismaMock = db as unknown as DeepMockProxy<PrismaClient>;
+
+export function pagifyMock(route: string, mock_obj: any): EligibilityFormPage {
+  return {
+    form_route: route,
+    form_data: mock_obj as Prisma.JsonObject,
+    updated_at: new Date(),
+    eligibility_form_page_id: "fakepageID",
+    eligibility_form_id: "fakeFormID",
+  } as EligibilityFormPage;
+}
--- tests/utils/db.server.test.ts
+++ tests/utils/db.server.test.ts
@@ -1,11 +1,21 @@
 import {
   findClinicByName,
   findClinics,
+  findEligibilityPages,
+  updateEligibility,
+  upsertEligibilityAndEligibilityPage,
   ZipClinicDistanceWithClinic,
 } from "app/utils/db.server";
-import { prismaMock } from "tests/helpers/prismaMock";
-import { getMockChooseClinicData } from "tests/helpers/mockData";
+import { prismaMock, pagifyMock } from "tests/helpers/prismaMock";
+import {
+  getMockChooseClinicData,
+  getMockEligibilityData,
+  getMockContactData,
+  getMockIncomeData,
+  getMockSession,
+} from "tests/helpers/mockData";
 import { Clinic, ZipClinicDistance } from "@prisma/client";
+import { EligibilityPagesType } from "app/types";
 
 it("returns a clinic record", async () => {
   const mockClinic = getMockChooseClinicData();
@@ -37,4 +47,156 @@ it("flattens clinic details into the result object", async () => {
       ...mockClinic,
     },
   ]);
+});
+
+it("hydrates eligibility page records into an eligibilityForm with types", async () => {
+  // This test ensures that the DB helper returns something that looks like a session
+  // from a list of page records in the database
+  const mockClinicPage = pagifyMock("choose-clinic", getMockChooseClinicData());
+  const mockIncomePage = pagifyMock("income", getMockIncomeData());
+  const mockEligibilityPage = pagifyMock(
+    "eligibility",
+    getMockEligibilityData()
+  );
+  const mockContactPage = pagifyMock("contact", getMockContactData());
+
+  prismaMock.eligibilityFormPage.findMany.mockResolvedValue([
+    mockClinicPage,
+    mockIncomePage,
+    mockEligibilityPage,
+    mockContactPage,
+  ]);
+  const result = await findEligibilityPages("fakeFormID");
+  expect(prismaMock.eligibilityFormPage.findMany).toHaveBeenCalledWith(
+    expect.objectContaining({
+      where: { eligibility_form_id: "fakeFormID" },
+    })
+  );
+  expect(result).toEqual(getMockSession());
+});
+
+it("creates a form page and eligibility page", async () => {
+  const mockEligibilityPage = getMockEligibilityData();
+  const eligibilityID = "test eligibility ID";
+  const formRoute = "eligibility";
+  prismaMock.eligibilityForm.upsert.mockResolvedValue({
+    eligibility_form_id: eligibilityID,
+    submitted: false,
+    completed: false,
+    updated_at: new Date(),
+  });
+  prismaMock.eligibilityFormPage.create.mockResolvedValue({
+    eligibility_form_id: eligibilityID,
+    form_route: formRoute,
+    form_data: mockEligibilityPage,
+    eligibility_form_page_id: "bogus page ID",
+    updated_at: new Date(),
+  });
+  await upsertEligibilityAndEligibilityPage(
+    eligibilityID,
+    formRoute,
+    mockEligibilityPage
+  );
+  expect(prismaMock.eligibilityForm.upsert).toHaveBeenCalledWith(
+    expect.objectContaining({
+      where: { eligibility_form_id: eligibilityID },
+      create: { eligibility_form_id: eligibilityID },
+    })
+  );
+  expect(prismaMock.eligibilityFormPage.findFirst).toHaveBeenCalledWith({
+    where: {
+      eligibility_form_id: eligibilityID,
+      form_route: formRoute,
+    },
+  });
+  expect(prismaMock.eligibilityFormPage.update).not.toBeCalled;
+  expect(prismaMock.eligibilityFormPage.create).toHaveBeenCalledWith({
+    data: {
+      eligibility_form_id: eligibilityID,
+      form_route: formRoute,
+      form_data: mockEligibilityPage,
+    },
+  });
+});
+
+it("creates a form page and updates an existing eligibility page", async () => {
+  const mockEligibilityPage = getMockEligibilityData();
+  const mockIncomePage = getMockIncomeData();
+  const eligibilityID = "test eligibility ID";
+  const formRoute = "eligibility";
+  prismaMock.eligibilityForm.upsert.mockResolvedValue({
+    eligibility_form_id: eligibilityID,
+    submitted: false,
+    completed: false,
+    updated_at: new Date(),
+  });
+  prismaMock.eligibilityFormPage.findFirst.mockResolvedValue({
+    eligibility_form_id: eligibilityID,
+    form_route: formRoute,
+    form_data: mockIncomePage,
+    eligibility_form_page_id: "bogus page ID",
+    updated_at: new Date(),
+  });
+  await upsertEligibilityAndEligibilityPage(
+    eligibilityID,
+    formRoute,
+    mockEligibilityPage
+  );
+  expect(prismaMock.eligibilityForm.upsert).toHaveBeenCalledWith(
+    expect.objectContaining({
+      where: { eligibility_form_id: eligibilityID },
+      create: { eligibility_form_id: eligibilityID },
+    })
+  );
+  expect(prismaMock.eligibilityFormPage.findFirst).toHaveBeenCalledWith({
+    where: {
+      eligibility_form_id: eligibilityID,
+      form_route: formRoute,
+    },
+  });
+  expect(prismaMock.eligibilityFormPage.update).toHaveBeenCalledWith({
+    where: { eligibility_form_page_id: "bogus page ID" },
+    data: { form_data: mockEligibilityPage },
+  });
+  expect(prismaMock.eligibilityFormPage.create).not.toBeCalled;
+});
+
+it("updates a form if it finds one", async () => {
+  const eligibilityID = "test eligibility ID";
+  prismaMock.eligibilityForm.findUnique.mockResolvedValue({
+    eligibility_form_id: eligibilityID,
+    submitted: false,
+    completed: false,
+    updated_at: new Date(),
+  });
+  await updateEligibility(eligibilityID, true, true);
+  expect(prismaMock.eligibilityForm.findUnique).toHaveBeenCalledWith(
+    expect.objectContaining({
+      where: {
+        eligibility_form_id: eligibilityID,
+      },
+    })
+  );
+  expect(prismaMock.eligibilityForm.update).toHaveBeenCalledWith({
+    where: {
+      eligibility_form_id: eligibilityID,
+    },
+    data: {
+      completed: true,
+      submitted: true,
+    },
+  });
+});
+
+it("does not update a form if it doesn't find one", async () => {
+  const eligibilityID = "test eligibility ID";
+  await updateEligibility(eligibilityID, true, true);
+  expect(prismaMock.eligibilityForm.findUnique).toHaveBeenCalledWith(
+    expect.objectContaining({
+      where: {
+        eligibility_form_id: eligibilityID,
+      },
+    })
+  );
+  expect(prismaMock.eligibilityForm.update).not.toBeCalled;
 });
